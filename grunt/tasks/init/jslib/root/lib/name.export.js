70f86fe6e78fe6b44da47bf410e9a1a7cc65c77e
Thanasis Polychronakis <thanpolas@gmail.com>
2012-06-27 22:49:32 +0300

    Added grunt templates

---
+++ grunt/tasks/init/jslib/root/lib/name.export.js
@@ -0,0 +1,11 @@
+// exports 
+
+goog.provide('ss.???');
+goog.require('ss.{%= name %}');
+
+/**
+ * @type {ss.{%= name %}}
+ */
+// ss.server = ss.Server2js.getInstance();
+
+// goog.exportSymbol('ss.server', ss.server);
